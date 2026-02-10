996ac5d52389e729e2fa1b891a688e2b908b50a9
UMAR • FAROOQ <ufprince1@gmail.com>
2026-02-10 10:49:45 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
-module.exports = {
+export default {
   HF_TOKEN: process.env.HF_TOKEN,
-
   HF_MODEL:
-    process.env.HF_MODEL ||
-    "microsoft/DialoGPT-medium",
-
+    process.env.HF_MODEL || "microsoft/DialoGPT-medium",
   AI_TIMEOUT: process.env.AI_TIMEOUT
     ? Number(process.env.AI_TIMEOUT)
     : 30000,
